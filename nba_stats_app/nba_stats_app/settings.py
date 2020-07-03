992b4efc5fbad406af39971a4034d9727bb398b3
Daniel Rodriguez <daniel.andres.rodriguez@gmail.com>
2020-07-03 12:20:12 -0700

    Repository cleanup.

--- nba_stats_app/nba_stats_app/settings.py
+++ nba_stats_app/nba_stats_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = 'qvn&qu6@kibj2akrsml(_y^_bb_-i6lj9)ky7d9_a(_(y6_pch'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nbashotcharts.herokuapp.com']
 
 
 # Application definition
@@ -118,4 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+
+django_heroku.settings(locals())
