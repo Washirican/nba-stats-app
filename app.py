23ca482fa6e364a4acffeaa35f5ad9f2d1c2fb67
Daniel Rodriguez <daniel.andres.rodriguez@gmail.com>
2021-04-06 14:10:35 -0700

    Add code to get player list.

--- app.py
+++ app.py
@@ -6,10 +6,27 @@ from utils import get_http_response
 
 app = Flask(__name__)
 
+HEADERS = {
+        'Host': 'stats.nba.com',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) '
+                      'Gecko/20100101 Firefox/72.0',
+        'Accept': 'application/json, text/plain, */*',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'x-nba-stats-origin': 'stats',
+        'x-nba-stats-token': 'true',
+        'Connection': 'keep-alive',
+        'Referer': 'https://stats.nba.com/',
+        'Pragma': 'no-cache',
+        'Cache-Control': 'no-cache',
+        }
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    player_index_url = 'https://stats.nba.com/js/data/ptsd/stats_ptsd.js'
+    data = get_http_response(player_index_url)
+    return render_template('index.html', data=data)
 
 
 if __name__ == '__main__':
