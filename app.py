1c98e975da07bf6f1dc959499026042ade7c7efb
Daniel Rodriguez <daniel.andres.rodriguez@gmail.com>
2021-04-14 11:17:04 -0700

    Add player profile flask route and template.

--- app.py
+++ app.py
@@ -3,19 +3,20 @@
 from datetime import datetime, timedelta
 from rq import Queue
 from worker import conn
-from flask import Flask
+from flask import Flask, render_template, request, Response
 from utils import *
+
+app = Flask(__name__)
 
 q = Queue(connection=conn)
 
-player_id = 201939
 
-
-def queue_tasks():
+def queue_tasks(player_id):
     result = q.enqueue(get_player_common_info, player_id)
 
     # Keep checking job status until it is finished
     while result.get_status() != 'finished':
+        # TODO (D. Rodriguez 2021-04-14): Revise this
         print('Waiting for results...')
 
     player_common_info = result.result[0]
@@ -23,9 +24,24 @@ def queue_tasks():
     return player_common_info, player_headline_stats
 
 
-if __name__ == "__main__":
-    print('Starting everything...')
-    player_common_info, player_headline_stats = queue_tasks()
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    """"""
+    player_id = 201939
 
-    print(player_common_info)
-    print(player_headline_stats)
+    player_common_info, player_headline_stats = queue_tasks(player_id)
+
+    return render_template('player_profile.html',
+                           player_common_info=player_common_info,
+                           player_headline_stats=player_headline_stats)
+
+
+if __name__ == "__main__":
+    # print('Starting everything...')
+    # player_common_info, player_headline_stats = queue_tasks()
+    #
+    # print(player_common_info)
+    # print(player_headline_stats)
+
+    app.run(debug=True)
+
