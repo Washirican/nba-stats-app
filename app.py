382579f42be93848b681f4770455b4267c88c0da
Daniel Rodriguez <daniel.andres.rodriguez@gmail.com>
2022-09-15 21:36:20 -0700

    Add player list template.

--- app.py
+++ app.py
@@ -8,32 +8,29 @@ from utils import *
 
 app = Flask(__name__)
 
-q = Queue(connection=conn)
-
-
-def queue_tasks(player_id):
-    result = q.enqueue(get_player_common_info, player_id)
-
-    # Keep checking job status until it is finished
-    while result.get_status() != 'finished':
-        # TODO (D. Rodriguez 2021-04-14): Revise this
-        print('Waiting for results...')
-
-    player_common_info = result.result[0]
-    player_headline_stats = result.result[1]
-    return player_common_info, player_headline_stats
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     """"""
-    player_id = 201939
+    # player_id = 201939
 
-    player_common_info, player_headline_stats = queue_tasks(player_id)
+    # player_common_info, player_headline_stats = get_player_common_info(player_id)
 
-    return render_template('player_profile.html',
-                           player_common_info=player_common_info,
-                           player_headline_stats=player_headline_stats)
+    player_index_url = "https://stats.wnba.com/js/data/ptsd/stats_ptsd.js"
+    player_list = requests.get(player_index_url)
+    player_data = json.loads(player_list.content.decode()[17:-1])
+
+    # Extracts data from dictionary
+    players = player_data['data']['players']
+    teams = player_data['data']['teams']
+    data_date = player_data['generated']
+
+    return render_template('index.html',
+                           content=players)
+
+    # return render_template('player_profile.html',
+    #                        player_common_info=player_common_info,
+    #                        player_headline_stats=player_headline_stats)
 
 
 if __name__ == "__main__":
@@ -44,4 +41,3 @@ if __name__ == "__main__":
     # print(player_headline_stats)
 
     app.run(debug=True)
-
